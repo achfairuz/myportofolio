06082535c3e33270d521e46641dfc5fbd962b277
achfairuz <fairf717@gmail.com>
2025-03-27 23:23:57 +0700

    updatee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iyus</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('iyuz.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('storage/iyuz.png') }}">
     @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <style>
